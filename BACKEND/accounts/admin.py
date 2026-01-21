f0468d3d2fae9e18eb7760a903a7c3e3325d1869
mwaitukasteven <mwaitukasteven@gmail.com>
2026-01-22 01:40:21 +0300

    add the models to the admin panel

--- BACKEND/accounts/admin.py
+++ BACKEND/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Studentprofile, Companyprofile  
 
 # Register your models here.
+admin.site.register(Studentprofile)
+admin.site.register(Companyprofile) 
